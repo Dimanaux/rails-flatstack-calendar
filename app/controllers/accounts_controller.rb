a7c333436225f2f44a8670f4890e17243777f745
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-08-27 08:19:29 +0300

    personal calendar

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,9 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: [:show]
+  before_action :set_from_and_to, only: [:show]
 
   def show
-    @events = @account.events
+    @days = Repeat.map_days_to_repeats(@from..@to, @account)
   end
 
   private
@@ -11,7 +12,19 @@ class AccountsController < ApplicationController
     @account = Account.find(params[:id])
   end
 
-  def account_params
-    params.require(:account).permit(:id)
+  def accounts_params
+    params.require(:account).permit(:id, :year_and_month)
+  end
+
+  def set_from_and_to
+    today = Date.today
+    @from = Date.new(today.year, today.month)
+    @from = parse_date(params[:year_and_month]) if params[:year_and_month]
+    @to = @from + 34.days
+  end
+
+  def parse_date(str)
+    year, month = str.split '-'
+    Date.new(year.to_i, month.to_i)
   end
 end
