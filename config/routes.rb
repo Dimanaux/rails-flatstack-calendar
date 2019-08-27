f8dbaef237ed926693571ff4e8679344fec8a14d
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-08-27 08:00:22 +0300

    redirect from / to /calendar/

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :events
   devise_for :accounts
 
-  get 'calendar(/:year_and_month)', to: 'calendar#show', name: 'calendar'
-  root to: 'calendar#show'
+  get 'calendar(/:year_and_month)', to: 'calendar#show', as: :calendar_path
+  root to: redirect('/calendar')
 
   resources :accounts, only: [:show]
 end
