ce96ee0bdb1155b80c5cb8444cd1b5c86cac8fa0
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-08-27 08:10:01 +0300

    add email presence validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,5 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :delete_all
+  validates :email, presence: true
 end
